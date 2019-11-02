b19855a3fae93dc94f12f9ddfa7504d35d4365bb
patrick.winkler <patrick.winkler@matchmycolor.com>
2019-11-02 14:31:01 +0100

    Added login page.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Login from "./components/Login";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Switch>
+          <Route path="/login">
+            <Login/>
+          </Route>
           <Route path="/profile">
             Profile
           </Route>
