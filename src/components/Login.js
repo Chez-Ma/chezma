a7eaa7655de6f6feaa3d42656b4b8f6618de0de8
patrick.winkler <patrick.winkler@matchmycolor.com>
2019-11-02 16:27:44 +0100

    Added content for login page.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,40 @@
 import React, {Component} from 'react';
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+
+const mail = "test";
+const password = "password";
 
 class Login extends Component {
   render() {
     return (
-      <div>
-        Login
-      </div>
+      <form noValidate autoComplete="off">
+        <div>
+          <TextField
+            id="user-mail"
+            label="E-Mail"
+            margin="normal"
+            type="test"
+            value={mail}
+            required
+          />
+        </div>
+        <div>
+          <TextField
+            id="user-pw"
+            label="Password"
+            margin="normal"
+            type="password"
+            value={password}
+            required
+          />
+        </div>
+        <div>
+          <Button variant="contained" color="primary">
+            Login
+          </Button>
+        </div>
+      </form>
     );
   }
 }
