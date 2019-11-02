b1077fbeb68e347c3d9b2d1d2e1cd22ae0ebcd0d
TobiasBeck777 <tobias.beck.89@gmail.com>
2019-11-02 16:51:21 +0100

    Added actions.

---
+++ src/actions/actions.js
@@ -0,0 +1,3 @@
+export const REGISTER = 'REGISTER'
+export const LOGIN = 'LOGIN'
+export const SAVE_PROFILE = 'SAVE_PROFILE'
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,37 +1,38 @@
 import React, {Component} from 'react';
-import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
-const mail = "test";
-const password = "password";
+const mail = 'test';
+const password = 'password';
+
 
 class Register extends Component {
     render() {
         return (
             <div>
-                <form noValidate autoComplete="off">
+                <form noValidate autoComplete='off'>
                     <div>
                         <TextField
-                            id="user-mail"
-                            label="E-Mail"
-                            margin="normal"
-                            type="test"
+                            id='user-mail'
+                            label='E-Mail'
+                            margin='normal'
+                            type='test'
                             value={mail}
                             required
                         />
                     </div>
                     <div>
                         <TextField
-                            id="user-pw"
-                            label="Password"
-                            margin="normal"
-                            type="password"
+                            id='user-pw'
+                            label='Password'
+                            margin='normal'
+                            type='password'
                             value={password}
                             required
                         />
                     </div>
                     <div>
-                        <Button variant="contained" color="primary">
+                        <Button variant='contained' color='primary'>
                             Register
                         </Button>
                     </div>
